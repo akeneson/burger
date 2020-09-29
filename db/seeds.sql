b66c1eae797930c247a39263aa6c94334d17c26d
Amy Keneson <amy.l.keneson@icloud.com>
2020-09-28 21:01:14 -0500

    seeds.sql added

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Hamburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Turkey Burger");
